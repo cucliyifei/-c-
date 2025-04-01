e7cd973bfa94b70f14ce854e9368abae17c59f3c
LiYifei-AI <lyf15637345629@qq.com>
2025-04-01 09:44:28 +0800

    Update 子数整数.cpp

--- 子数整数.cpp
+++ 子数整数.cpp
@@ -1,3 +1,11 @@
+/*对于一个五位数  a1a2a3a4a5,可将其拆分为三个子数：
+sub1=a1a2a3,sub2=a2a3a4,sub3=a3a4a5
+例如，五位数 20207 可以拆分成
+sub1=202
+sub2=020 (=20)
+sub3=207
+现在给定一个正整数 K，要求你编程求出 10000 到 30000 之间所有满足下述条件的五位数，条件是这些五位数的三个子数 sub1,sub2,sub3
+​都可被 K 整除。
 #include <iostream>
 #include <cstdio>
 using namespace std;
